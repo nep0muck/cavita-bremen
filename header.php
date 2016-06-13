23c36cf1302785a203fe3a1452cfd10a0c4ad54b
nep0muck <me@eichlersolutions.de>
2016-06-13 23:43:24 +0200

    Inserted logo as svg

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header container" role="banner">
 		<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-		
+
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12">
@@ -34,25 +34,25 @@
 					            <span class="icon-bar"></span>
 					            <span class="icon-bar"></span>
 							</button>
-				            
-				            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"  class="navbar-brand"><i class="fa fa-leaf"></i> <?php bloginfo( 'name' ); ?></a>
-				            
+
+				            <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"  class="navbar-brand"><?php include('logo.php'); ?></a>
+
 				        </div>
-						
-						<?php 
-						$args = array('theme_location' => 'primary', 
-									  'container_class' => 'navbar-collapse collapse', 
+
+						<?php
+						$args = array('theme_location' => 'primary',
+									  'container_class' => 'navbar-collapse collapse',
 									  'menu_class' => 'nav navbar-nav',
 									  'fallback_cb' => '',
 			                          'menu_id' => 'main-menu',
-			                          'walker' => new Upbootwp_Walker_Nav_Menu()); 
+			                          'walker' => new Upbootwp_Walker_Nav_Menu());
 						wp_nav_menu($args);
 						?>
 					</div><!-- .col-md-12 -->
 				</div><!-- row -->
 			</div><!-- container -->
 		</nav>
-		
+
 	</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
